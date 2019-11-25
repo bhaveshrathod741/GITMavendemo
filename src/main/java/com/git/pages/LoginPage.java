b8418322f5779098148d185000e824d77b863da1
bhaveshrathod741 <sameer.rathod29@gmail.com>
2019-11-26 00:20:45 +0530

    first commit

---
+++ src/main/java/com/git/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package com.git.pages;
+
+public class LoginPage {
+	
+	 public void login()
+	 {
+		 System.out.println("Login page");
+
+	}
+
+}
---
+++ src/test/java/com/git/testpages/LoginTest.java
@@ -0,0 +1,10 @@
+package com.git.testpages;
+
+public class LoginTest {
+
+	@Test
+	public void loginTest()
+	{
+		System.out.println("Login Test");
+	}
+}
